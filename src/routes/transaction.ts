a096d5b2f8f0e2d52250721a895fa487965c5bd8
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-09-18 23:05:40 +0800

    fix: timeout issue on fetch

--- src/routes/transaction.ts
+++ src/routes/transaction.ts
@@ -7,34 +7,36 @@ import { drizzle } from "drizzle-orm/neon-http";
 const transaction = new Hono<{ Bindings: CloudflareBindings }>();
 const alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const nanoid = customAlphabet(alphabet, 25);
-const baseURL = "https://test.dragonpay.ph/api/collect/v1";
 
 transaction.get("/", c => {
   return c.text("ok");
 });
 
 transaction.post("/create", async c => {
-  const body = await c.req.json();
+  const body = JSON.stringify(await c.req.json());
 
-  const pgsql = neon(c.env.DATABASE_URL);
-  const db = drizzle({ client: pgsql });
+  const token = btoa(`${c.env.DP_MERCHANT_ID}:${c.env.DP_MERCHANT_API_KEY}`);
 
-  const authToken = btoa(
-    `${c.env.DP_MERCHANT_ID}:${c.env.DP_MERCHANT_API_KEY}`
-  );
-  try {
-    const request = await fetch(`${baseURL}/${nanoid()}/post`, {
+  const response = await fetch(
+    `https://test.dragonpay.ph/api/collect/v1/${nanoid()}/post`,
+    {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
-        Authorization: `Basic ${authToken}`
+        Authorization: `Basic ${token}`,
+        "Content-Type": "application/json; charset=UTF8"
       },
-      body: JSON.stringify(body)
-    });
-  } catch (error) {
-    console.log(error);
-  }
-  return c.text("ok");
+      body
+    }
+  );
+
+  const data = await response.json<{
+    refno: string;
+    status: string;
+    message: string;
+    url: string;
+  }>();
+
+  return c.json(data);
 });
 
 export default transaction;
